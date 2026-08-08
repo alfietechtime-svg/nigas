a910a811fd6d36653dc989013f10735438b1cf24
alfietechtime-svg <alfietechtime@gmail.com>
2026-08-08 16:28:11 +0100

    Update faggot.lua

--- faggot.lua
+++ faggot.lua
@@ -1,4 +1,4 @@
-on this local Players = game:GetService("Players")
+local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local Workspace = game:GetService("Workspace")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
